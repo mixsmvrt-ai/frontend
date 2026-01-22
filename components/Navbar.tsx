c1173a582c7db12e3b923d4f593b7933ed767e21
mixsmvrt-ai <mixsmvrt@gmail.com>
2026-01-21 21:27:24 -0500

    Add files via upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import type { User } from "@supabase/supabase-js";
